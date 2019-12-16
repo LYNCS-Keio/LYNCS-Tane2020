ac9565654e4d31d9ff03b0d1f7c95d7c67d4fb72
shun-nagayama <allandarp.shun@gmail.com>
2019-12-16 17:48:16 +0900

    send_recv

--- raspberry_pi/twelite/module.cpp
+++ raspberry_pi/twelite/module.cpp
@@ -1,19 +1,7 @@
 #include <pybind11/pybind11.h>
-#include "twelite.hpp"
+#include "send_recv.cpp"
 
-namespace py = pybind11;
-
-PYBIND11_MODULE(twelite, t) {
-    py::module m("twe_twe", "pybind11 plugin");
-    py::class_<TWE_Lite>(t, "twe_lite")
-        .def(py::init())
-        .def("send_simple", &TWE_Lite::send_simple)
-        .def("send_buf_simple", &TWE_Lite::send_buf_simple)
-        .def("do_send", &TWE_Lite::do_send)
-        .def("check_send", &TWE_Lite::check_send)
-        .def("get_data", &TWE_Lite::get_data)
-        .def("recv", &TWE_Lite::recv)
-        .def("recv", &TWE_Lite::recv)
-        .def("try_recv", &TWE_Lite::try_recv)
-        .def("try_recv8", &TWE_Lite::try_recv8)
+PYBIND11_MODULE(twelite_module, m) {
+    m.doc() = "twelite module test";
+    m.def("send_recv", &send_recv, "send and recv");
 }
