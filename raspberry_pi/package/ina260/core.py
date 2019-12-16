6a79ce0ae7bbf543a7ccb768a29afb77a5e4bdc2
NeM-T <persodou@keio.jp>
2019-12-16 16:42:01 +0900

    class できてる？

--- raspberry_pi/package/ina260/core.py
+++ raspberry_pi/package/ina260/core.py
@@ -2,32 +2,45 @@
 
 import pigpio
 
-pi = pi.pigpio()
-ina_addr     = 0x44
-config_addr  = 0x00
-current_addr = 0x01
-vol_addr     = 0x02
-lsb          = 1.25
+class ina:
+    pi = pi.pigpio()
+    ina_addr     = 0x44
+    config_addr  = 0x00
+    current_addr = 0x01
+    vol_addr     = 0x02
+    lsb          = 1.25
 
-ina = pi.i2c_open(1, ina_addr)
+    try:
+        ina = pi.i2c_open(1, ina_addr)
+    except:
+        print("Failed initializing ina260")
 
-def get_vol():
-    raw = pi.i2c_read_i2c_block_data(ina, vol_addr, 2)
-    word_data = raw[0] *256 + raw[1]
-    vol = float(word_data) / 1000 * lsb
-    return vol 
+    def get_vol(self):
+        try:
+            raw = self.pi.i2c_read_i2c_block_data(self.ina_addr, self.vol_addr, 2)
+        except:
+            print("Failed getting vol_data ina260")
+        word_data = raw[0] *256 + raw[1]
+        vol = float(word_data) / 1000 * self.lsb
+        return vol 
 
-def get_current():
-    raw = pi.i2c_read_i2c_block_data(ina, current_addr, 2)
-    word_data = raw[0] *256 + raw[1]
-    current_sign_bit = word_data >> 15
-    if (current_sign_bit == 1 and word_data & 1 << 15):
-        word_data -= 1 << 16
+    def get_current(self):
+        try:
+            raw = self.pi.i2c_read_i2c_block_data(self.ina_addr, self.current_addr, 2)
+        except:
+            print("Failed getting current_data ina260")
+        word_data = raw[0] *256 + raw[1]
+        current_sign_bit = word_data >> 15
+        if (current_sign_bit == 1 and word_data & 1 << 15):
+            word_data -= 1 << 16
 
-    current = float(word_data) / 1000 * lsb
-    return current
+        current = float(word_data) / 1000 * self.lsb
+        return current
 
-def reset_chip():
-    byte_list = [0x80, 0x00]
-    pi.i2c_write_i2c_block_data(ina_addr, config_addr, byte_list)
+    def reset_chip(self):
+        byte_list = [0x80, 0x00]
+        try:
+            self.pi.i2c_write_i2c_block_data(ina_addr, config_addr, byte_list)
+        except:
+            print("Failed reset_chip ina260")
 
