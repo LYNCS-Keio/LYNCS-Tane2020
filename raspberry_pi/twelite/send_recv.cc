5dd269c3032ccdccd98494a482ae26ea40f67c50
shun-nagayama <allandarp.shun@gmail.com>
2019-12-23 19:11:44 +0900

    typo

--- raspberry_pi/twelite/send_recv.cc
+++ raspberry_pi/twelite/send_recv.cc
@@ -4,17 +4,17 @@
 #define RASPBERRY_PI
 #include "twelite.hpp"
 
-TWE_Lite twelit("/dev/ttyS0", 115200);
+TWE_Lite twelite("/dev/ttyS0", 115200);
 
 int send_recv(){
-	twelit.init();
+	twelite.init();
 
 	uint8_t buf[] = { 'A', 'B' };
 	while(true){
 		std::cout << "send: ";
-//		twelit.recv();
-		twelit.send_buf_extend(0x00, 0x01, buf, 2);
-		if(twelit.check_send())
+//		twelite.recv();
+		twelite.send_buf_extend(0x00, 0x01, buf, 2);
+		if(twelite.check_send())
 			std::cout << "ok";
 		else
 			std::cout << "fail";
@@ -22,11 +22,11 @@ int send_recv(){
 
 		delay(100);
 continue;
-		const size_t size = twelit.recv();
+		const size_t size = twelite.recv();
 		if(size == 0) continue;
 		std::cout << "recv: ";
 		for(size_t i=0;i<size;i++){
-//			std::cout << std::hex << (uint32_t)twelit.recv_buf[i];
+//			std::cout << std::hex << (uint32_t)twelite.recv_buf[i];
 		}
 		std::cout << std::endl;
 	}
