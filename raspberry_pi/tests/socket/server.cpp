ceb124dee0652e37ebb6899f11c098cfa25220f5
shun-nagayama <allandarp.shun@gmail.com>
2019-12-25 17:58:56 +0900

    fix

--- raspberry_pi/tests/socket/server.cpp
+++ raspberry_pi/tests/socket/server.cpp
@@ -1,5 +1,3 @@
-// https://www.geekpage.jp/programming/linux-network/book/07/7-9.php
-
 #define FILEPATH "./test.sock"
 #include "server.h"
 
@@ -13,58 +11,10 @@ char buf[1024];
 int flag;
 int n;
 
-
-void setup() {
-  remove(FILEPATH);
-
-  if (s0 < 0) {
-    perror("socket");
-    return 1;
-  }
-  s_un.sun_family = AF_UNIX;
-
-  strcpy(s_un.sun_path, FILEPATH);
-
-  if (bind(s0, (struct sockaddr *)&s_un, sizeof(s_un)) != 0) {
-    perror("bind");
-    return 1;
-  }
-
-  if (listen(s0, 5) != 0) {
-    perror("listen");
-    return 1;
-  }
+int main() {
+    setup();
+    accept();
+    read();
+    write();
+    unlink();
 }
-
-void accept() {
-  addrlen = sizeof(s_un_accept);
-
-  sock = accept(s0, (struct sockaddr *)&s_un_accept, &addrlen);
-  if (sock < 0) {
-    perror("accept");
-    return 1;
-  }
-
-  printf("after accept\n");
-}
-
-void read() {
-  memset(buf, 0, sizeof(buf));
-  n = read(sock, buf, sizeof(buf));
-}
-
-void write() {
-  printf ("%s\n", buf);
-  write(sock, "HOGE", 4);
-
-  close(sock);
-  close(s0);
-}
-
-
-void unlink() {
-  unlink(FILEPATH);
-
-  return 0;
-}
-
--- raspberry_pi/tests/socket/server.h
+++ raspberry_pi/tests/socket/server.h
@@ -1,11 +1,60 @@
+// https://www.geekpage.jp/programming/linux-network/book/07/7-9.php
+
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
 
-void setup() {}
-void accept() {}
-void read() {}
-void write() {}
-void unlink() {}
+void setup() {
+  remove(FILEPATH);
+
+  if (s0 < 0) {
+    perror("socket");
+    return 1;
+  }
+  s_un.sun_family = AF_UNIX;
+
+  strcpy(s_un.sun_path, FILEPATH);
+
+  if (bind(s0, (struct sockaddr *)&s_un, sizeof(s_un)) != 0) {
+    perror("bind");
+    return 1;
+  }
+
+  if (listen(s0, 5) != 0) {
+    perror("listen");
+    return 1;
+  }
+}
+
+void accept() {
+  addrlen = sizeof(s_un_accept);
+
+  sock = accept(s0, (struct sockaddr *)&s_un_accept, &addrlen);
+  if (sock < 0) {
+    perror("accept");
+    return 1;
+  }
+
+  printf("after accept\n");
+}
+
+void read() {
+  memset(buf, 0, sizeof(buf));
+  n = read(sock, buf, sizeof(buf));
+}
+
+void write() {
+  printf ("%s\n", buf);
+  write(sock, "HOGE", 4);
+
+  close(sock);
+  close(s0);
+}
+
+void unlink() {
+  unlink(FILEPATH);
+
+  return 0;
+}
