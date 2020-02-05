fec7791d95eb937bff7a3e5efb0deeb9e8b41b14
RyosukeSasaki <rsasaki@keio.jp>
2020-02-05 20:51:49 +0900

    いいのか？

--- raspberry_pi/package/ina260/core.py
+++ raspberry_pi/package/ina260/core.py
@@ -3,21 +3,22 @@
 import pigpio
 
 class ina:
-    pi = pigpio.pi()
-    ina_addr     = 0x44
-    config_addr  = 0x00
-    current_addr = 0x01
-    vol_addr     = 0x02
-    lsb          = 1.25
+    def __init__(self):
+        self.pi = pigpio.pi()
+        ina_addr     = 0x40
+        self.config_addr  = 0x00
+        self.current_addr = 0x01
+        self.vol_addr     = 0x02
+        self.lsb          = 1.25
 
-    try:
-        ina = pi.i2c_open(1, ina_addr)
-    except:
-        print("Failed initializing ina260")
+        try:
+            self.bus = self.pi.i2c_open(1, ina_addr)
+        except:
+            print("Failed initializing ina260")
 
     def get_vol(self):
         try:
-            raw = self.pi.i2c_read_i2c_block_data(self.ina_addr, self.vol_addr, 2)
+            raw = self.pi.i2c_read_i2c_block_data(self.bus, self.vol_addr, 2)[1]
         except:
             print("Failed getting vol_data ina260")
         word_data = raw[0] *256 + raw[1]
@@ -26,7 +27,7 @@ class ina:
 
     def get_current(self):
         try:
-            raw = self.pi.i2c_read_i2c_block_data(self.ina_addr, self.current_addr, 2)
+            raw = self.pi.i2c_read_i2c_block_data(self.bus, self.current_addr, 2)
         except:
             print("Failed getting current_data ina260")
         word_data = raw[0] *256 + raw[1]
@@ -40,7 +41,7 @@ class ina:
     def reset_chip(self):
         byte_list = [0x80, 0x00]
         try:
-            self.pi.i2c_write_i2c_block_data(ina_addr, config_addr, byte_list)
+            self.pi.i2c_write_i2c_block_data(self.bus, self.config_addr, byte_list)
         except:
             print("Failed reset_chip ina260")
 
