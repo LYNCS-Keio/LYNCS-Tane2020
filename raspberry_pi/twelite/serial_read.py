1949b02818dba8b87dd8cf8e945d7d177baea729
RyosukeSasaki <rsasaki@keio.jp>
2020-02-07 10:47:46 +0000

    serial

--- raspberry_pi/twelite/serial_read.py
+++ raspberry_pi/twelite/serial_read.py
@@ -3,6 +3,7 @@ import serial
 ser = serial.Serial('/dev/ttyS0', 115200)
 while True:
     line = ser.readline()
+    line.encode('utf-8')
     print(line)
 
 ser.close()
--- raspberry_pi/twelite/serial_send.py
+++ raspberry_pi/twelite/serial_send.py
@@ -2,5 +2,5 @@ import serial
 
 ser = serial.Serial('/dev/ttyS0', 115200)
 while True:
-    ser.write("HELLO!!!")
+    ser.write(b"HELLO!!!")
 ser.close()
