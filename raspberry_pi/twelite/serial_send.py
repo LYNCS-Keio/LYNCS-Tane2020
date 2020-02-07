089df3f818db5d667a2ce8813a66296f71ab3216
shun-nagayama <allandarp.shun@gmail.com>
2020-02-07 20:30:04 +0900

    serial

--- raspberry_pi/twelite/serial_send.py
+++ raspberry_pi/twelite/serial_send.py
@@ -2,6 +2,6 @@ import serial
 
 ser = serial.Serial('/dev/ttyS0', 115200)
 while True:
-    data = bytes.fromhex(format(0xA55A8005000011223300, 'x'))
+    data = bytes.fromhex(format(0xA5 5A 80 05 00 00 11 22 33 00, 'x'))
     ser.write(data)
 ser.close()
