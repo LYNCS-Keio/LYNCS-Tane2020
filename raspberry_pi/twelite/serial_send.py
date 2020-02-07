8a51803a448fa6444291afd3714e0028fdb0dc95
shun-nagayama <allandarp.shun@gmail.com>
2020-02-07 20:36:04 +0900

    serial

--- raspberry_pi/twelite/serial_send.py
+++ raspberry_pi/twelite/serial_send.py
@@ -2,6 +2,7 @@ import serial
 
 ser = serial.Serial('/dev/ttyS0', 115200)
 while True:
-    data = binascii.a2b_hex('0xA55A8005000011223300')
+    # data = bytes.fromhex(format(0xA55A8005000011223300, 'x'))
+    data = "0xA50x5A0x800x050x000x000x110x220x330x00"
     ser.write(data)
 ser.close()
