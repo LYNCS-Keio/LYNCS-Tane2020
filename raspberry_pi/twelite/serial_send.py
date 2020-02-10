e2d9dfdfec9f6ae8c771955801b4ec60e6d6e1ab
RyosukeSasaki <rsasaki@keio.jp>
2020-02-10 09:29:30 +0000

    success

--- raspberry_pi/twelite/serial_send.py
+++ raspberry_pi/twelite/serial_send.py
@@ -8,10 +8,10 @@ h1 = pi.serial_open("/dev/ttyS0", 115200)
 #ser = serial.Serial('/dev/ttyS0', 115200)
 while True:
     #data = ':0001112233AABBCCXX\r\n'
-    
-    pi.serial_write(h1, b'hello!')
-    ser.write(data)
-    print(data)
+
+    pi.serial_write(h1, ':0001112233AABBCCXX\r\n')
+    #ser.write(data)
+    #print(data)
     time.sleep(0.1)
 
 ser.close()
