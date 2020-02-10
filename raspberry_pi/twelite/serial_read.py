0442c6945d386987e696d4d72f68e7eedba0b3dd
RyosukeSasaki <rsasaki@keio.jp>
2020-02-10 07:29:19 +0000

    serial

--- raspberry_pi/twelite/serial_read.py
+++ raspberry_pi/twelite/serial_read.py
@@ -5,5 +5,6 @@ while True:
     line = ser.readline()
     print(line)
     print(line.decode('UTF-8'))
+    print(int(line), 16)
 
 ser.close()
--- raspberry_pi/twelite/serial_send.py
+++ raspberry_pi/twelite/serial_send.py
@@ -4,15 +4,10 @@ import time
 ser = serial.Serial('/dev/ttyS0', 115200)
 while True:
     # data = bytes.fromhex(format(0xA55A8005000011223300, 'x'))
-    #data = 'abcd'
-    #data2 = data.encode('utf-8')
-    #ser.write(data2)
-    #print(data2)
-    #time.sleep(0.1)
-    data = ':7880010101FFFFFFFFFFFFFFFFXX'+ '\r\n'
-    ser.write(data.encode('UTF-8'))
+
+    data = bytes.fromhex('abcd')
+    ser.write(data)
     print(data)
-    print(data.encode('UTF-8'))
     time.sleep(0.1)
 
 ser.close()
