5c5618f028911fee1406aa208f9106e26e946ad0
shun-nagayama <allandarp.shun@gmail.com>
2020-02-13 21:56:22 +0900

    DB

--- raspberry_pi/twelite/sand_box.py
+++ raspberry_pi/twelite/sand_box.py
@@ -5,8 +5,19 @@ import time
 pi = pigpio.pi()
 h1 = pi.serial_open("/dev/ttyS0", 115200)
 
-data = ':DBF306X'
-pi.serial_write(h1, data)
+# data = ':DBF306X'
+# pi.serial_write(h1, data)
+
+buf = [0xA5, 0x5A, 0xF2, 0x06, 0x02]
+
+checksum = 0x00
+for i in range(len(buf)):
+    checksum = checksum ^ buf[i]
+
+pi.serial_write(h1, [0xA5, 0x5A, 0xF2, 0x06, 0x02] + [checksum])
+print(buf + [checksum])
+
+time.sleep(1)
 
 line = pi.serial_read(h1)
 print(line)
