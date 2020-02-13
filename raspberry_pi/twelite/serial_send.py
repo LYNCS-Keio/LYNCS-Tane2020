154223bcb95e6f266f0069750e4166c9927fd097
shun-nagayama <allandarp.shun@gmail.com>
2020-02-13 17:58:44 +0900

    serial_send

--- raspberry_pi/twelite/serial_send.py
+++ raspberry_pi/twelite/serial_send.py
@@ -12,39 +12,40 @@ header = [0xA5, 0x5A]
 
 while True:
 # binary
-    # buf = 0x112233AABBCC
-    # header = 0xA55A
-    # checksum = 0x00
-    # for i in range(len(header)):
-    #     checksum = checksum ^ header[i]
-    # for i in range(len(buf)):
-    #     checksum = checksum ^ buf[i]
+    buf = 0x112233AABBCC
+    header = 0xA55A
+    checksum = 0x00
+    for i in range(len(header)):
+        checksum = checksum ^ header[i]
+    for i in range(len(buf)):
+        checksum = checksum ^ buf[i]
     
-    # cmd_size = len(header) + len(buf)
-    # cmd = 0x8000 + cmd_size
+    cmd_size = len(header) + len(buf)
+    cmd = 0x8000 + cmd_size
 
+    pi.serial_write(h1, header + [0x80, 0x08] + buf + [checksum])
     # pi.serial_write(h1, header)
     # pi.serial_write(h1, [0x80, 0x08])
-    # # pi.serial_write(h1, cmd >> 8 & 0b1111)
-    # # pi.serial_write(h1, cmd >> 4 & 0b1111)
-    # # pi.serial_write(h1, cmd & 0b1111)
+    # pi.serial_write(h1, cmd >> 8 & 0b11111111)
     # pi.serial_write(h1, buf)
     # pi.serial_write(h1, [checksum])
     
-    # print(h1, header)
-    # print(h1, cmd >> 8)
-    # print(h1, cmd & 0b11111111)
-    # print(h1, buf)
-    # print(h1, [checksum])
+    print(h1, header)
+    print(h1, cmd >> 8)
+    print(h1, cmd & 0b11111111)
+    print(h1, buf)
+    print(h1, [checksum])
 
 
 
 
 # ascii
-    data = ':0001112233AABBCCXX\r\n'
-    pi.serial_write(h1, data)
-    # ser.write(data)
-    # print(data)
+    # data = ':0001112233AABBCCXX\r\n'
+    # pi.serial_write(h1, data)
+    # # ser.write(data)
+    # # print(data)
+
+
     time.sleep(0.1)
 
 ser.close()
