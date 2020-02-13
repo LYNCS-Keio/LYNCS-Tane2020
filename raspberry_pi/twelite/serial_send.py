3f226c72634c870eaf2b9424c874885d91cbade3
shun-nagayama <allandarp.shun@gmail.com>
2020-02-13 18:27:47 +0900

    serial_send

--- raspberry_pi/twelite/serial_send.py
+++ raspberry_pi/twelite/serial_send.py
@@ -28,7 +28,7 @@ while True:
     # pi.serial_write(h1, buf)
     # pi.serial_write(h1, [checksum])
 
-    print(header + [0x80, 0x08] + buf + [checksum])
+    print(header + [0x80, 0x08] + buf + [checksum] + [0x04])
     # print(header)
     # print(cmd >> 8)
     # print(cmd & 0b11111111)
