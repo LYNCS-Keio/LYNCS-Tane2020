2761a727432f3f2d083df43ffe8e7c75550505e4
nagayama3 <allandarp.shun@gmail/.com>
2020-02-13 12:45:23 +0000

    DB

--- raspberry_pi/twelite/sand_box.py
+++ raspberry_pi/twelite/sand_box.py
@@ -5,5 +5,8 @@ import time
 pi = pigpio.pi()
 h1 = pi.serial_open("/dev/ttyS0", 115200)
 
-data = ':DBF20602\r\n'
+data = ':DBF20602X'
 pi.serial_write(h1, data)
+
+line = pi.serial_read(h1, 100)
+print(line)
