0557a10caf95fbb88709fdf31fa24d143b1b22b4
shun-nagayama <allandarp.shun@gmail.com>
2020-02-13 16:36:38 +0900

    serial_send

---
+++ raspberry_pi/twelite/sand_box.py
@@ -0,0 +1,23 @@
+buf = [0x11, 0x22, 0x33, 0xAA, 0xBB, 0xCC]
+header = [0xA5, 0x5A]
+
+checksum = 0x00
+for i in range(len(header)):
+    checksum = checksum ^ header[i]
+for i in range(len(buf)):
+    checksum = checksum ^ buf[i]
+
+cmd_size = len(header) + len(buf)
+
+print(header)
+print(0x8000 + cmd_size)
+print(format(0x8000 + cmd_size, 'x'))
+print(buf)
+print(checksum)
+
+print(size)
+print(buf)
+# print(len(str(format(buf, 'x'))))
+# print(format(buf, 'x'))
+# print(format(18838593387468, 'x'))
+print(size_calculate(buf))
--- raspberry_pi/twelite/serial_send.py
+++ raspberry_pi/twelite/serial_send.py
@@ -6,12 +6,33 @@ pi = pigpio.pi()
 h1 = pi.serial_open("/dev/ttyS0", 115200)
 
 #ser = serial.Serial('/dev/ttyS0', 115200)
-while True:
-    #data = ':0001112233AABBCCXX\r\n'
 
-    pi.serial_write(h1, ':0001112233AABBCCXX\r\n')
-    #ser.write(data)
-    #print(data)
+buf = [0x11, 0x22, 0x33, 0xAA, 0xBB, 0xCC]
+header = [0xA5, 0x5A]
+
+while True:
+# binary
+    # buf = 0x112233AABBCC
+    # header = 0xA55A
+    checksum = 0x00
+    for i in range(len(header)):
+        checksum = checksum ^ header[i]
+    for i in range(len(buf)):
+        checksum = checksum ^ buf[i]
+    
+    cmd_size = len(header) + len(buf)
+
+    pi.serial_write(h1, header)
+    pi.serial_write(h1, 0x8000 + cmd_size)
+    pi.serial_write(h1, buf)
+    pi.serial_write(h1, checksum)
+
+
+# ascii
+    # data = ':0001112233AABBCCXX\r\n'
+    # pi.serial_write(h1, data)
+    # ser.write(data)
+    # print(data)
     time.sleep(0.1)
 
 ser.close()
