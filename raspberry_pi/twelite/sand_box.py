72c5fd8951d7cb0d2e7205d13ae01f8845ac5c42
shun-nagayama <allandarp.shun@gmail.com>
2020-02-13 21:34:18 +0900

    DB

--- raspberry_pi/twelite/sand_box.py
+++ raspberry_pi/twelite/sand_box.py
@@ -1,36 +1,9 @@
-buf = [0x11, 0x22, 0x33, 0xAA, 0xBB, 0xCC]
-header = [0xA5, 0x5A]
+import serial
+import pigpio
+import time
 
-checksum = 0x00
-for i in range(len(header)):
-    checksum = checksum ^ header[i]
-for i in range(len(buf)):
-    checksum = checksum ^ buf[i]
+pi = pigpio.pi()
+h1 = pi.serial_open("/dev/ttyS0", 115200)
 
-cmd_size = len(header) + len(buf)
-
-print(cmd_size)
-cmd = 0x8000 + cmd_size
-print(cmd)
-print([cmd >> 8, cmd & 0b111111111])
-print(bin(cmd >> 8))
-
-
-
-
-
-
-
-
-# print(header)
-# print(0x8000 + cmd_size)
-# print(format(0x8000 + cmd_size, 'x'))
-# print(buf)
-# print(checksum)
-
-# print(size)
-# print(buf)
-# print(len(str(format(buf, 'x'))))
-# print(format(buf, 'x'))
-# print(format(18838593387468, 'x'))
-# print(size_calculate(buf))
+data = ':DBF2062\r\n'
+pi.serial_write(h1, data)
