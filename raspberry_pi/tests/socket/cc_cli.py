3a85e3e659979afca93ffd7bd7b2f7a4ef361690
NeM-T <persodou@keio.jp>
2020-02-18 20:24:24 +0900

    関数化・float対応

--- raspberry_pi/tests/socket/cc_cli.py
+++ raspberry_pi/tests/socket/cc_cli.py
@@ -3,25 +3,32 @@
 from socket import socket, AF_UNIX, SOCK_STREAM
 import sys
 import time
+import struct
 
-def sock_cli(path, msg):
-    if type(msg) is str:
-        msg = msg.encode()
+class pycli:
+    def  __init__ (self, path):
 
-    if type(msg) is int:
-        msg = msg.to_bytes(4, "little")
+        self.s = socket(AF_UNIX, SOCK_STREAM)
+        self.s.connect(path)
 
-    s = socket(AF_UNIX, SOCK_STREAM)
-    s.connect(path)
+    def sock_cli(self, msg):
+        if type(msg) is str:
+            msg = msg.encode()
+        elif type(msg) is int:
+            msg = msg.to_bytes(4, "little")
+        elif type(msg) is float:
+            msg = struct.pack('>d', msg)
 
-    while True:
+
         sys.stdout.write("send to server ({}): {}\n".format(1, msg))
-        s.send(msg)
-        data = s.recv(1024)
+        self.s.send(msg)
+        data = self.s.recv(1024)
         sys.stdout.write("receive from server: {}\n".format(data.decode()))
 
-    s.close()
-    print (repr(data))
+    def __del__(self):
+        self.s.close()
 
 if __name__ == "__main__":
-    sock_cli("test.sock", 80)
+    so = pycli("test.sock")
+    while 1:
+        so.sock_cli(80.5)
