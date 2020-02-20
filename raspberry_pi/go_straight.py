80101c5e6759ad15a0437bbf293c9f550d8b12f9
RyosukeSasaki <rsasaki@keio.jp>
2020-02-20 11:37:14 +0000

    地磁気を使わなければうまくいった。

--- raspberry_pi/go_straight.py
+++ raspberry_pi/go_straight.py
@@ -22,17 +22,17 @@ pi.set_mode(12, pigpio.OUTPUT)
 pi.write(11, 0)
 pi.write(9, 0)
 pi.write(10, 0)
-svL, svR = pi.hardware_PWM(13, 50, 75000), pi.hardware_PWM(12, 50, 75000)
+#svL, svR = pi.hardware_PWM(13, 50, 75000), pi.hardware_PWM(12, 50, 75000)
 
 
 icm = icm20948.icm20948(pi)
 p = pid_controll.pid(0.004, 0.03, 0.0004)
 #p = pid_controll.pid(4.8, 23.65, 0.2436)
-mad = madgwick_py.MadgwickAHRS(0.05)
+mad = madgwick_py.MadgwickAHRS()
 pt = time.time()
 
-log_list = [logger.logger_list_t.ICM_MAG]
-logger = logger.logger(log_list, '/home/pi/LYNCS-Tane2020/raspberry_pi/'+ 'log_1' +'.csv')
+log_list = [logger.logger_list_t.ICM_MADGWICK]
+logger = logger.logger(log_list, '/home/pi/LYNCS-Tane2020/raspberry_pi/'+ 'log_5' +'.csv')
 
 try:
     while True:
@@ -40,16 +40,20 @@ try:
         mx, my, mz = icm.read_magnetometer_data()
         gx, gy, gz = gx*PI/180, gy*PI/180, gz*PI/180
 
-        mad.update([gx,gy,gz], [ax,ay,az], [mx,-my,-mz])
+        #mad.update([gx,gy,gz], [ax,ay,az], [mx,-my,-mz])
+        mad.update_imu([gx,gy,gz],[ax,ay,az])
         x, y, z = mad.quaternion.to_euler_angles()
-        x, y, z = x*PI/180, y*PI/180, z*PI/180
-        gyro = z
+        x, y, z = x*180/PI, y*180/PI, z*180/PI
+        gyro = y
+        rotation = gyro
+        #print('{0:.2f} {0:.2f} {0:.2f}'.format(x,y,z))
+        print(x,y,z)
 
         nt = time.time()
         dt = nt - pt
         pt = nt
-        rotation += gyro * dt
-        m = p.update_pid(0, rotation, dt)
+        #rotation += gyro * dt
+        m = p.update_pid(0, gyro, dt)
 
         m1 = min([max([m, -1]), 1])
 
@@ -59,10 +63,10 @@ try:
         
         # print([m, dL, dR, rotation])
 
-        logger.printer()
-        logger.csv_logger()
+        #logger.printer()
+        #logger.csv_logger()
 
-        time.sleep(0.01)
+        #time.sleep(0.01)
 
 finally:
     pi.hardware_PWM(12, 0, 0)
