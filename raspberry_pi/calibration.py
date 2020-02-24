0d2d657a3f1f0852b6c4c64a49c071163f95e86c
shun-nagayama <allandarp.shun@gmail.com>
2020-02-24 19:06:19 +0900

    fix calibration

--- raspberry_pi/calibration.py
+++ raspberry_pi/calibration.py
@@ -1,4 +1,6 @@
 from package import icm20948
+import csv
+import os
 
 b = [0.0, 0.0, 0.0, 0.1] #[x, y, z, r]
 x = 0.0
@@ -17,13 +19,18 @@ def update_magnetometer_data(x, y, z):
     b[2] = b[2] + 4 * lr * f * dz
     b[3] = b[3] + 4 * lr * f * b[3]
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
 if __name__ == "__main__":
     import pigpio
     pi = pigpio.pi()
     imu = icm20948.icm20948(pi)
 
-    while True:
-        x, y, z = imu.read_magnetometer_data()
-        update_magnetometer_data(x, y, z)
-        print(b)
-        time.sleep(0.1)
+    while open(current_dir + '/' + 'log0001' + '.csv', 'w') as c:
+        f = csv.writer(c, lineterminator = '\n')
+        while True:
+            x, y, z = imu.read_magnetometer_data()
+            update_magnetometer_data(x, y, z)
+            b.append([x, y, z])
+            print(b)
+            f.writerow(b)
