ad9e10b638c12da4ff2edb962a67bb5408912c6e
RypsukeSasaki <rsasaki@keio.jp>
2020-03-04 10:52:33 +0000

    地磁気とジャイロの融合

--- raspberry_pi/go_straight.py
+++ raspberry_pi/go_straight.py
@@ -2,77 +2,129 @@
 
 from package import pid_controll
 from package import icm20948
-from package import madgwick_py
-import logger
+# from package import madgwick_py
 import pigpio
 import time
 from math import pi as PI
+from math import atan2
+from statistics import mean
 
-rotation = 0
-# drift = -1.032555
+
+def calibrate_mag(sec):
+    Flag = True
+    pi.hardware_PWM(12, 50, 75000)
+    pi.hardware_PWM(13, 50, 75000)
+    time.sleep(0.1)
+    s_t = time.time()
+    while Flag:
+        pi.hardware_PWM(13, 50, 90000)
+        pi.hardware_PWM(12, 50, 90000)
+        try:
+            x, y, z = icm.read_magnetometer_data()
+        except　KeyboardInterrupt:
+            raise KeyboardInterrupt
+        finally:
+            pass
+
+        y,z = -y,-z
+        
+        dx = x - b[0]
+        dy = y - b[1]
+        dz = z - b[2]
+        f = dx*dx + dy*dy + dz*dz - b[3]*b[3]
+        
+        b[0] = b[0] + 4 * lr * f * dx
+        b[1] = b[1] + 4 * lr * f * dy
+        b[2] = b[2] + 4 * lr * f * dz
+        b[3] = b[3] + 4 * lr * f * b[3]
+        if (time.time() - s_t) > sec:
+            Flag = False
+    pi.hardware_PWM(12, 50, 75000)
+    pi.hardware_PWM(13, 50, 75000)
+
+
+def calc_drift(sec):
+    pi.hardware_PWM(13, 50, 75000)
+    pi.hardware_PWM(12, 50, 75000)
+    Flag = True
+    gx_drift = []
+    gy_drift = []
+    gz_drift = []
+    time.sleep(0.1)
+    s_t = time.time()
+    while Flag:
+        try:
+            ax, ay, az, gx, gy, gz = icm.read_accelerometer_gyro_data()
+            gx_drift.append(gx)
+            gy_drift.append(gy)
+            gz_drift.append(gz)
+        except KeyboardInterrupt:
+            raise KeyboardInterrupt
+        finally:
+            pass
+        if (time.time() - s_t) > sec:
+            Flag = False
+    gx_drift = mean(gx_drift)
+    gy_drift = mean(gy_drift)
+    gz_drift = mean(gz_drift)
+
+
+def update_azimuth():
+    mx, my, mz = icm.read_magnetometer_data()
+    azimuth = atan2(my-b[1], mx-b[0])*180/PI
 
 
 pi = pigpio.pi()
 
 pi.set_mode(13, pigpio.OUTPUT)
 pi.set_mode(12, pigpio.OUTPUT)
-<<<<<<< HEAD
-pi.write(11, 0)
-pi.write(9, 0)
-pi.write(10, 0)
-#svL, svR = pi.hardware_PWM(13, 50, 75000), pi.hardware_PWM(12, 50, 75000)
-=======
-svL, svR = pi.hardware_PWM(13, 50, 75000), pi.hardware_PWM(12, 50, 75000)
->>>>>>> f19193eb10373e0c19abc6beb69b48ce60d4bec0
+pi.hardware_PWM(13, 50, 75000)
+pi.hardware_PWM(12, 50, 75000)
 
 
 icm = icm20948.icm20948(pi)
-p = pid_controll.pid(0.004, 0.03, 0.0004)
-<<<<<<< HEAD
-#p = pid_controll.pid(4.8, 23.65, 0.2436)
-mad = madgwick_py.MadgwickAHRS()
-=======
-#p = pid_controll.pid(4.8, 23.65, 0.2436) Tane19のゲイン
-mad = madgwick_py.MadgwickAHRS(0.05)
->>>>>>> f19193eb10373e0c19abc6beb69b48ce60d4bec0
-pt = time.time()
+p = pid_controll.pid(0.002, 0.005, 0.003)
+# mad = madgwick_py.MadgwickAHRS()
 
-log_list = [logger.logger_list_t.ICM_MADGWICK]
-logger = logger.logger(log_list, '/home/pi/LYNCS-Tane2020/raspberry_pi/'+ 'log_5' +'.csv')
+b = [30.0, 0.0, 15.0, 20]
+lr = 0.0001
+
+SPEED = 0.7
 
 try:
+    calibrate_mag(30)
+    update_azimuth()
+    calc_drift(30)
+    
+    pt = time.time()
     while True:
-        ax, ay, az, gx, gy, gz = icm.read_accelerometer_gyro_data()
-        mx, my, mz = icm.read_magnetometer_data()
-        gx, gy, gz = gx*PI/180, gy*PI/180, gz*PI/180
+        try:
+            ax, ay, az, gx, gy, gz = icm.read_accelerometer_gyro_data()
+        except KeyboardInterrupt:
+            raise KeyboardInterrupt
+        except:
+            gz = 0
+        finally:
+            pass
 
-        #mad.update([gx,gy,gz], [ax,ay,az], [mx,-my,-mz])
-        mad.update_imu([gx,gy,gz],[ax,ay,az])
-        x, y, z = mad.quaternion.to_euler_angles()
-        x, y, z = x*180/PI, y*180/PI, z*180/PI
-        gyro = y
-        rotation = gyro
-        #print('{0:.2f} {0:.2f} {0:.2f}'.format(x,y,z))
-        print(x,y,z)
+        # mad.update([gx,gy,gz], [ax,ay,az], [mx,-my,-mz])
+        # x, y, z = mad.quaternion.to_euler_angles()
+        # x, y, z = x*180/PI, y*180/PI, z*180/PI
 
         nt = time.time()
         dt = nt - pt
         pt = nt
-        #rotation += gyro * dt
-        m = p.update_pid(0, gyro, dt)
+        azimuth += gz*dt
+        m = p.update_pid(0, azimuth, dt)
 
         m1 = min([max([m, -1]), 1])
 
-        dL, dR = 75000 + 12500 * (1 - m1), 75000 - 12500 * (1 + m1)
+        dL, dR = 75000 + 12500 * (SPEED - m1), 75000 - 12500 * (SPEED + m1)
         pi.hardware_PWM(13, 50, int(dL))
         pi.hardware_PWM(12, 50, int(dR))
-        
-        # print([m, dL, dR, rotation])
 
-        #logger.printer()
-        #logger.csv_logger()
-
-        #time.sleep(0.01)
+except KeyboardInterrupt:
+    pass        
 
 finally:
     pi.hardware_PWM(12, 0, 0)
