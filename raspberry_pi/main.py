e4b5ab2926f686643554c4b6ca2b13b7cd8af6ce
RyosukeSasaki <rsasaki@keio.jp>
2020-03-16 20:04:56 +0900

    add camera option

--- raspberry_pi/main.py
+++ raspberry_pi/main.py
@@ -3,9 +3,9 @@ from package import dps310
 from package import icm20948
 from package import ina260
 from package import pid_controll
-#from package import twelite
-#from package import camera
-#from package import capture
+from package import twelite
+from package import camera
+from package import capture
 
 import time
 import pigpio
@@ -16,14 +16,23 @@ import run
 
 pi = pigpio.pi()
 icm = icm20948.icm20948(pi)
+dps = dps310(pi)
 toml_dic = toml.load(open('config.toml'))
 p = pid_controll.pid(toml_dic['PID']['P'], toml_dic['PID']['I'], toml_dic['PID']['D'])
+rotation_lock = threading.Lock()
 
-threshold_high = toml_dic['height']['high']
-threshold_low = toml_dic['height']['low']
-continuous_number = toml_dic['height']['continue_number']
+dps.set_OpMode(opMpde.IDLE)
+dps.get_coeffs()
+dps.config_Pressure(mesurement_conf.MEAS_RATE_16, measurement_conf.MEAS_RATE_16)
+dps.config_Temperature(mesurement_conf.MEAS_RATE_32, measurement_conf.MEAS_RATE_32)
+dps.set_OpMode(opMode.CONT_BOTH)
 
-camera_distance = toml_dic['camera']['start_distance']
+THRESHOLD_HIGH = toml_dic['height']['high']
+THRESHOLD_LOW = toml_dic['height']['low']
+CONTINUOUS_NUM = toml_dic['height']['continue_number']
+
+LIMIT_DISTANCE = toml_dic['camera']['start_distance']
+AoV = toml_dic['camera']['Angle_of_View']
 pinL = toml_dic['pin']['Rmotor']
 pinR = toml_dic['pin']['Lmotor']
 goal = [toml_dic['gps']['goal_lat'], toml_dic['gps']['goal_long']]
@@ -32,35 +41,21 @@ lr = toml_dic['icm_calib']['learning_rate']
 b = [30.0, 0.0, 15.0, 20]
 drift = [0,0,0]
 azimuth = 0
+to_goal = [None, None]
 
-def pressure_while(height_threshold, continuous_number, mode)
-    dps = dps310(pi, 0x77)
-    dps.set_OpMode(opMpde.IDLE)
-    dps.get_coeffs()
-    dps.config_Pressure(mesurement_conf.MEAS_RATE_16, measurement_conf.MEAS_RATE_16)
-    dps.config_Temperature(mesurement_conf.MEAS_RATE_32, measurement_conf.MEAS_RATE_32)
-    dps.set_OpMode(opMode.CONT_BOTH)
 
+def pressure_while(height_threshold, continuous_num, timeout, mode)
     n = 0
-    while n < continuous_number:
+    pt = time.time()
+    while n < continuous_num:
         height = dps.measurement_high()
         if (mode)*height >= (mode)*height_threshold:
             n += 1
         else:
             n = 0
         time.sleep(0.01)
-
-try:
-    pressure_while(threshold_high, continuous_number, 1)
-    release_time = time.time() # 落下開始
-    pressure_while(threshold_low, continuous_number, -1)
-
-except KeyboardInterrupt:
-    pass        
-
-finally:
-    pi.hardware_PWM(12, 0, 0)
-    pi.hardware_PWM(13, 0, 0)
+        if (time.time() - pt) > timeout:
+            break
 
 
 def update_target_gps():
@@ -69,6 +64,22 @@ def update_target_gps():
         now = gps.lat_long_measurement()
         if now != [None, None]:
             to_goal = gps.convert_lat_long_to_r_theta(*now, *goal)
+
+
+def update_rotation_with_cam():
+    global rotation
+    cap = capture.capture()
+    cam = camera.CamAnalysis()
+    while UPDATE_CAMERA == True:
+        stream = cap.cap()
+        cam.morphology_extract(stream)
+        cam.save_all_outputs()
+        coord = cam.contour_find()
+
+        conX = ((coord[0] - width / 2) / (width / 2)) / math.sqrt(3)
+        rotation_lock.acquire()
+        rotation = math.degrees(math.atan(-conX))
+        rotation_lock.release()
 
 
 try:
@@ -87,35 +98,70 @@ try:
     GPS_THREAD = threading.Thread(target=update_target_gps)
     GPS_THREAD.start()
     
-    speed = 0.7
-    pt = time.time()
-    while True:
-        try:
-            ax, ay, az, gx, gy, gz = icm.read_accelerometer_gyro_data()
-        except KeyboardInterrupt:
-            raise KeyboardInterrupt
-        except:
-            gz = 0
-        finally:
-            pass
-        
-        nt = time.time()
-        dt = nt - pt
-        pt = nt
-        azimuth += gz*dt
-        m = p.update_pid(target_azimuth, azimuth, dt)
 
-        m1 = min([max([m, -1]), 1])
+    if STATE = 0:        
+        pressure_while(THRESHOLD_HIGH, CONTINUOUS_NUM, 300, 1)
+        pressure_while(THRESHOLD_LOW, CONTINUOUS_NUM, 300, -1)
+        pi.write(26)
+    
 
-        dL, dR = 75000 + 12500 * (speed - m1), 75000 - 12500 * (speed + m1)
-        pi.hardware_PWM(13, 50, int(dL))
-        pi.hardware_PWM(12, 50, int(dR))
+    elif STATE = 1:
+        pt = time.time()
+        while True:
+            try:
+                ax, ay, az, gx, gy, gz = icm.read_accelerometer_gyro_data()
+            except KeyboardInterrupt:
+                raise KeyboardInterrupt
+            except:
+                gz = 0
+            finally:
+                pass
+            
+            nt = time.time()
+            dt = nt - pt
+            pt = nt
+            azimuth += gz*dt
+            m = p.update_pid(target_azimuth, azimuth, dt)
 
-        if to_goal[0] <= camera_distance:
-            break
+            m1 = min([max([m, -1]), 1])
+
+            dL, dR = 75000 + 12500 * (speed - m1), 75000 - 12500 * (speed + m1)
+            pi.hardware_PWM(13, 50, int(dL))
+            pi.hardware_PWM(12, 50, int(dR))
+
+            if to_goal[0] <= LIMIT_DISTANCE:
+                break
+
+
+    elif STATE = 2:
+        pt = time.time()
+        while True:
+            try:
+                ax, ay, az, gx, gy, gz = icm.read_accelerometer_gyro_data()
+            except KeyboardInterrupt:
+                raise KeyboardInterrupt
+            except:
+                gz = 0
+            finally:
+                pass
+            
+            nt = time.time()
+            dt = nt - pt
+            pt = nt
+            rotation += gz*dt
+            m = p.update_pid(0, rotation, dt)
+
+            m1 = min([max([m, -1]), 1])
+
+            dL, dR = 75000 + 12500 * (speed - m1), 75000 - 12500 * (speed + m1)
+            pi.hardware_PWM(13, 50, int(dL))
+            pi.hardware_PWM(12, 50, int(dR))
+
+
 
 except KeyboardInterrupt:
     pass        
+
 
 finally:
     UPDATE_GPS = False
